75af546cf0a1887ffc46349191f1241713eae074
EbukaTheDeveloper <ebukaigweonu8@gmail.com>
2024-02-13 08:39:32 +0100

    modify myApp.js

--- myApp.js
+++ myApp.js
@@ -1,9 +1,10 @@
 let express = require('express');
 let app = express();
-console.log('Hello World');
+console.log(__dirname);
 
 app.get("/",(req,res) => {
-    res.send('Hello Express');
+    const absolutePath = __dirname + '/views/index.html';
+    res.sendFile(absolutePath);
 })
 
 
